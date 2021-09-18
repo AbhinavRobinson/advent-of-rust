d62c0a27386c6fe4d225f37d3ae9dd3d8091bedd
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-09-18 11:54:30 +0530

    (feat): generate all pairs

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,10 @@
+use anyhow::Result;
+
 fn main() -> anyhow::Result<()> {
     let input: Vec<i64> = include_str!("input.txt")
         .split('\n')
         .map(str::parse::<i64>)
-        .map(Result::unwrap)
-        .collect();
+        .collect::<Result<Vec<_>, _>>()?;
 
     let result = pair_sum_is_x(&input, 3488);
     dbg!(result);
@@ -11,15 +12,23 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn pair_sum_is_x(s: &[i64], x: i64) -> Option<(i64, i64)> {
+    for (a, b) in all_pairs(s) {
+        if a + b == x {
+            return Some((a, b));
+        }
+    }
+    None
+}
+
+fn all_pairs(s: &[i64]) -> Vec<(i64, i64)> {
+    let mut pairs: Vec<_> = Default::default();
     for i in 0..s.len() {
         for j in 0..s.len() {
             if i == j {
                 continue;
             }
-            if s[i] + s[j] == x {
-                return Some((s[i], s[j]));
-            }
+            pairs.push((s[i], s[j]));
         }
     }
-    None
+    pairs
 }
