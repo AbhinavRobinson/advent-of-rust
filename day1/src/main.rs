ebf9857d54fc0206952c43b2852d1bb3c86db771
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-09-18 11:59:10 +0530

    (feat): add itertools

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use itertools::Itertools;
 
 fn main() -> anyhow::Result<()> {
     let input: Vec<i64> = include_str!("input.txt")
@@ -11,24 +12,6 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn pair_sum_is_x(s: &[i64], x: i64) -> Option<(i64, i64)> {
-    for (a, b) in all_pairs(s) {
-        if a + b == x {
-            return Some((a, b));
-        }
-    }
-    None
-}
-
-fn all_pairs(s: &[i64]) -> Vec<(i64, i64)> {
-    let mut pairs: Vec<_> = Default::default();
-    for i in 0..s.len() {
-        for j in 0..s.len() {
-            if i == j {
-                continue;
-            }
-            pairs.push((s[i], s[j]));
-        }
-    }
-    pairs
+fn pair_sum_is_x(s: &[i64], x: i64) -> Option<(&i64, &i64)> {
+    s.iter().tuple_combinations().find(|(a, b)| *a + *b == x)
 }
