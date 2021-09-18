80de8c4d8cc8f4091dc0dc11164464700df5574a
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-09-18 11:45:31 +0530

    (feat): day1 find pair sum

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,25 @@
 fn main() -> anyhow::Result<()> {
-    let s = include_str!("input.txt");
-    dbg!(&s[..40]);
+    let input: Vec<i64> = include_str!("input.txt")
+        .split('\n')
+        .map(str::parse::<i64>)
+        .map(Result::unwrap)
+        .collect();
 
+    let result = pair_sum_is_x(&input, 3488);
+    dbg!(result);
     Ok(())
+}
+
+fn pair_sum_is_x(s: &[i64], x: i64) -> Option<(i64, i64)> {
+    for i in 0..s.len() {
+        for j in 0..s.len() {
+            if i == j {
+                continue;
+            }
+            if s[i] + s[j] == x {
+                return Some((s[i], s[j]));
+            }
+        }
+    }
+    None
 }
