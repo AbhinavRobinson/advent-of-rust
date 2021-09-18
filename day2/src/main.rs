d79484fa26b03bdfa7ba494e636e0375f0cd5772
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-09-18 13:26:51 +0530

    (feat): add basic layout

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,22 @@
+use std::ops::RangeInclusive;
+
+struct PasswordPolicy {
+    byte: u8,
+    range: RangeInclusive<usize>,
+}
+
+impl PasswordPolicy {
+    fn isValid(&self, password: &str) -> bool {
+        todo!()
+    }
+}
+
+fn parse_input(input: &str) -> anyhow::Result<(PasswordPolicy, &str)> {
+    todo!()
+}
+
 fn main() -> anyhow::Result<()> {
+    let input  = include_str!("input.txt");
+
     Ok(())
 }
