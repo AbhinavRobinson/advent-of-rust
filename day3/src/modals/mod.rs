53622f8e15e0628d9e1481a36f03dfcc5c332008
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-09-25 12:35:56 +0530

    (feat): add enum for tile type

--- day3/src/modals/mod.rs
+++ day3/src/modals/mod.rs
@@ -1,5 +1,8 @@
 mod test;
 
+/// coordiantes {x,y}
+/// ---
+/// Description - Vector-2d are i64 2d coordinate type {x,y}
 #[derive(Debug, Clone, Copy, PartialEq)]
 struct Vec2 {
     x: i64,
@@ -7,7 +10,20 @@ struct Vec2 {
 }
 
 impl From<(i64, i64)> for Vec2 {
+    /// Converts i64 tuple to Vec2 type.
     fn from((x, y): (i64, i64)) -> Self {
         Self { x, y }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum Tile {
+    Open,
+    Tree,
+}
+
+impl Default for Tile {
+    fn default() -> Self {
+        Self::Open
     }
 }
